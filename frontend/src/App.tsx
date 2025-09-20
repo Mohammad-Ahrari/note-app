2252c98626e06ea1007407b1e29ea20b56a7d088
Mohammad-Ahrari <mahrari504@gmail.com>
2025-09-21 01:05:35 +0330

    feat(router): setup basic routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+
 const App = () => {
   return (
-    <div>
-      <h1>Note App</h1>
+    <div className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<h1>Create a New Note</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
